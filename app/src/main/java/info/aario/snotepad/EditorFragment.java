c03c953535cfcad626e3a507ac91e7372d4a6e69
Aario Shahbany <aario@users.noreply.github.com>
2017-03-19 16:19:42 +0100

    Implemented undo redo feature

--- app/src/main/java/info/aario/snotepad/EditorFragment.java
+++ app/src/main/java/info/aario/snotepad/EditorFragment.java
@@ -9,7 +9,10 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 /**
  * Created by aario on 3/16/17.
@@ -22,6 +25,58 @@ public class EditorFragment extends Fragment {
     private EditText etEditor;
     private EditText etTitle;
     private boolean modified;
+    ArrayList<String> textUndoHistory = new ArrayList<String>();
+    ArrayList<Integer> selectionStartUndoHistory = new ArrayList<Integer>();
+    ArrayList<Integer> selectionEndUndoHistory = new ArrayList<Integer>();
+    private Button btUndo;
+    private Button btRedo;
+    private int undoHistoryCursor = 0;
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            saveUndoRedo();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            modified = true;
+        }
+    };
+
+    private void saveUndoRedo() {
+        if (textUndoHistory.size() > 0 && undoHistoryCursor < textUndoHistory.size() - 1) {
+            for (int c = textUndoHistory.size() - 1; c >= undoHistoryCursor; c--) {
+                textUndoHistory.remove(c);
+                selectionStartUndoHistory.remove(c);
+                selectionEndUndoHistory.remove(c);
+            }
+        }
+        textUndoHistory.add(etEditor.getText().toString());
+        selectionStartUndoHistory.add(etEditor.getSelectionStart());
+        selectionEndUndoHistory.add(etEditor.getSelectionEnd());
+        undoHistoryCursor = textUndoHistory.size() - 1;
+        updateUndoRedoButtons();
+    }
+
+    private void retrieveUndoRedo() {
+        etEditor.removeTextChangedListener(textWatcher);
+        etEditor.setText(textUndoHistory.get(undoHistoryCursor).toString());
+        etEditor.setSelection(
+                selectionStartUndoHistory.get(undoHistoryCursor),
+                selectionStartUndoHistory.get(undoHistoryCursor)
+        );
+        etEditor.addTextChangedListener(textWatcher);
+        updateUndoRedoButtons();
+    }
+
+    private void updateUndoRedoButtons() {
+        btUndo.setEnabled(textUndoHistory.size() > 0 && undoHistoryCursor > 0);
+        btRedo.setEnabled(textUndoHistory.size() > 0 && undoHistoryCursor < textUndoHistory.size() - 1);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,22 +98,27 @@ public class EditorFragment extends Fragment {
         etEditor = (EditText) view.findViewById(R.id.etEditor);
         if (activity.filer.exists(path))
             etEditor.setText(activity.filer.getStringFromFile(path));
-        etEditor.addTextChangedListener(new TextWatcher() {
+        etEditor.addTextChangedListener(textWatcher);
+        textUndoHistory.clear();
+        selectionStartUndoHistory.clear();
+        selectionEndUndoHistory.clear();
+        btUndo = (Button) view.findViewById(R.id.btUndo);
+        btUndo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                modified = true;
+            public void onClick(View view) {
+                undoHistoryCursor--;
+                retrieveUndoRedo();
             }
         });
+        btRedo = (Button) view.findViewById(R.id.btRedo);
+        btRedo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                undoHistoryCursor++;
+                retrieveUndoRedo();
+            }
+        });
+        saveUndoRedo();
         return view;
     }
 
@@ -72,7 +132,7 @@ public class EditorFragment extends Fragment {
         }
 
         if (activity.filer.writeToFile(path, etEditor.getText().toString())) {
-            activity.makeSnackBar("Changes saved to "+path);
+            activity.makeSnackBar("Changes saved to " + path);
             if (rename)
                 activity.filer.delete(oldPath);
             modified = false;
