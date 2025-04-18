f1c4a8cc90eb4a96faed5a9867efe2c927e12b4a
Aario Shahbany <aario.shahbany@gmail.com>
2025-04-18 17:25:07 +0200

    Made google web fonts accessible locally

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -13,6 +13,7 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require("gulp-uglify");
+const googleWebfonts = require("gulp-google-webfonts");
 
 const fs = require('fs');
 const path = require('path');
@@ -32,28 +33,39 @@ const banner = ['/*!\n',
 // --- Configuration ---
 const paths = {
  templates: {
-   src: './templates/**/*.html', // Source HTML templates
-   dest: './js/build/',          // Destination for the generated JS file
-   generatedFile: '_templates.js' // Name of the generated file
+  src: './templates/**/*.html',
+  dest: './js/build/',
+  generatedFile: '_templates.js'
  },
  js: {
-   src: './js/*.js',             // Source *custom* JS files (excluding generated)
-   dest: './js/build/',          // Destination for final JS files
-   concatFile: 'scripts.js',     // Name of the concatenated file
-   minSuffix: '.min'             // Suffix for minified file
+  src: './js/*.js',
+  dest: './js/build/',
+  concatFile: 'scripts.js',
+  minSuffix: '.min'
+ },
+ // --- Fonts Configuration ---
+ fonts: {
+   src: './fonts.list',
+   dest: './', // Destination for gulp.dest() after googleWebfonts runs
+   fontDir: 'fonts/', // Fonts will go to ./fonts/ relative to dest
+   cssDir: 'css/',    // CSS will go to ./css/ relative to dest
+   cssFilename: 'google-fonts.css',
+   cssPath: '../fonts/' // <--- Correct relative path from CSS file to font directory
  }
+ // --- End Fonts Configuration ---
 };
-const templatesDir = path.join(__dirname, 'templates'); // Absolute path to templates dir
+const templatesDir = path.join(__dirname, 'templates');
 const generatedTemplatesJsPath = path.join(__dirname, paths.templates.dest, paths.templates.generatedFile);
+const generatedFontCssPath = path.join(__dirname, paths.fonts.cssDir, paths.fonts.cssFilename); // Path to generated font CSS
 
 // BrowserSync
 function browserSync(done) {
  browsersync.init({
-   server: {
-     baseDir: "./"
-   },
-   port: 3000,
-   open: false,
+  server: {
+   baseDir: "./"
+  },
+  port: 3000,
+  open: false,
  });
  done();
 }
@@ -64,188 +76,199 @@ function browserSyncReload(done) {
  done();
 }
 
-// Clean vendor and built assets
+// Clean vendor, built assets, and fonts
 function clean() {
  return del([
-   "./vendor/",
-   "./css/*.min.*",
-   "./js/build/*"
+  "./vendor/",
+  "./css/*.css",
+  "./js/build/*",
+  "./fonts/"
  ]);
 }
 
-// Bring third party dependencies from node_modules into vendor directory - MODIFIED
+// Google Fonts Task - CORRECTED
+function googlefonts() {
+  const fontListPath = paths.fonts.src;
+  if (!fs.existsSync(fontListPath)) {
+    const fontContent = 'Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i';
+    fs.writeFileSync(fontListPath, fontContent);
+    console.log(`Created ${fontListPath} with default Nunito font.`);
+  }
+
+  return gulp.src(paths.fonts.src)
+    .pipe(plumber()) // Add plumber for error handling
+    .pipe(googleWebfonts({
+      fontsDir: paths.fonts.fontDir, // Relative to dest
+      cssFilename: paths.fonts.cssFilename,
+      // formats: ['woff2', 'woff'] // Optionally specify formats
+    }))
+    .on('error', function(err) { // More specific error logging
+      console.error('Error in googlefonts task:', err.toString());
+      this.emit('end'); // Prevent Gulp from stopping on error
+    })
+    .pipe(gulp.dest(paths.fonts.dest)); // Output to root directory './'
+}
+
+
+// Bring third party dependencies from node_modules into vendor directory
 function modules() {
  // Bootstrap JS
  var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
-   .pipe(gulp.dest('./vendor/bootstrap/js'));
+  .pipe(gulp.dest('./vendor/bootstrap/js'));
  // Bootstrap SCSS
  var bootstrapSCSS = gulp.src('./node_modules/bootstrap/scss/**/*')
-   .pipe(gulp.dest('./vendor/bootstrap/scss'));
+  .pipe(gulp.dest('./vendor/bootstrap/scss'));
  // ChartJS
  var chartJS = gulp.src('./node_modules/chart.js/dist/*.js')
-   .pipe(gulp.dest('./vendor/chart.js'));
+  .pipe(gulp.dest('./vendor/chart.js'));
  // dataTables
  var dataTables = gulp.src([
-     './node_modules/datatables.net/js/*.js',
-     './node_modules/datatables.net-bs4/js/*.js',
-     './node_modules/datatables.net-bs4/css/*.css'
+   './node_modules/datatables.net/js/*.js',
+   './node_modules/datatables.net-bs4/js/*.js',
+   './node_modules/datatables.net-bs4/css/*.css'
    ])
-   .pipe(gulp.dest('./vendor/datatables'));
+  .pipe(gulp.dest('./vendor/datatables'));
  // Font Awesome
  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
-   .pipe(gulp.dest('./vendor'));
+  .pipe(gulp.dest('./vendor'));
  // jQuery Easing
  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-   .pipe(gulp.dest('./vendor/jquery-easing'));
+  .pipe(gulp.dest('./vendor/jquery-easing'));
  // jQuery
  var jquery = gulp.src([
-     './node_modules/jquery/dist/*',
-     '!./node_modules/jquery/dist/core.js'
+   './node_modules/jquery/dist/*',
+   '!./node_modules/jquery/dist/core.js'
    ])
-   .pipe(gulp.dest('./vendor/jquery'));
+  .pipe(gulp.dest('./vendor/jquery'));
  // EasyMDE
  var easymde = gulp.src([
-     './node_modules/easymde/dist/*'
+   './node_modules/easymde/dist/*'
    ])
-   .pipe(gulp.dest('./vendor/easymde'));
- // *** START Hammer.js ADDITION ***
- var hammerJS = gulp.src('./node_modules/hammerjs/hammer.min.js') // Select the minified file
-    .pipe(gulp.dest('./vendor/hammerjs')); // Copy it to vendor/hammerjs/
- // *** END Hammer.js ADDITION ***
+  .pipe(gulp.dest('./vendor/easymde'));
+ // Hammer.js
+ var hammerJS = gulp.src('./node_modules/hammerjs/hammer.min.js')
+  .pipe(gulp.dest('./vendor/hammerjs'));
 
- // *** Add hammerJS to the merge stream ***
- return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables, fontAwesome, jquery, jqueryEasing, easymde, hammerJS); // Added hammerJS here
+ return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables, fontAwesome, jquery, jqueryEasing, easymde, hammerJS);
 }
 
-// CSS task
+// CSS task - Includes generated font CSS
 function css() {
- return gulp
-   .src("./scss/**/*.scss")
-   .pipe(plumber())
-   .pipe(sass({
-     outputStyle: "expanded",
-     includePaths: "./node_modules",
-   }))
-   .on("error", sass.logError)
-   .pipe(autoprefixer({
-     cascade: false
-   }))
-   .pipe(header(banner, {
-     pkg: pkg
-   }))
-   .pipe(gulp.dest("./css"))
-   .pipe(rename({
-     suffix: ".min"
-   }))
-   .pipe(cleanCSS())
-   .pipe(gulp.dest("./css"))
-   .pipe(browsersync.stream());
+ // Process SCSS separately first
+ var scssStream = gulp.src("./scss/styles.scss") // Process only the main SCSS file
+  .pipe(plumber())
+  .pipe(sass({
+   outputStyle: "expanded",
+   includePaths: "./node_modules",
+  }))
+  .on("error", sass.logError)
+  .pipe(autoprefixer({
+   cascade: false
+  }));
+ // Stream for the generated google fonts css
+ var fontCssStream = gulp.src(generatedFontCssPath, { allowEmpty: true });
+
+ // Merge the two streams, process together
+ return merge(fontCssStream, scssStream) // Font CSS first, then compiled SCSS
+  .pipe(plumber())
+  .pipe(concat('styles.css')) // Concatenate font CSS and compiled SCSS
+  .pipe(header(banner, {
+   pkg: pkg
+  }))
+  .pipe(gulp.dest("./css")) // Output the non-minified combined CSS
+  .pipe(rename({
+   suffix: ".min"
+  }))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("./css")) // Output the minified combined CSS
+  .pipe(browsersync.stream());
 }
+
 
 function generateTemplatesJS(done) {
  // (generateTemplatesJS function remains unchanged)
-  // Check if templates directory exists
- if (!fs.existsSync(templatesDir)) {
-   console.warn(`Templates directory not found: ${templatesDir}. Skipping template generation.`);
-   // Create an empty file to avoid errors in the 'js' task if it expects the file
-   if (!fs.existsSync(path.dirname(generatedTemplatesJsPath))) {
-     fs.mkdirSync(path.dirname(generatedTemplatesJsPath), { recursive: true });
-   }
-   fs.writeFileSync(generatedTemplatesJsPath, 'window.templates = {};\n');
-   return done();
+  if (!fs.existsSync(templatesDir)) {
+  console.warn(`Templates directory not found: ${templatesDir}. Skipping template generation.`);
+  if (!fs.existsSync(path.dirname(generatedTemplatesJsPath))) {
+   fs.mkdirSync(path.dirname(generatedTemplatesJsPath), { recursive: true });
+  }
+  fs.writeFileSync(generatedTemplatesJsPath, 'window.templates = {};\n');
+  return done();
  }
-
  let templatesObject = 'window.templates = {\n';
  const files = fs.readdirSync(templatesDir);
-
  files.forEach(file => {
-   const filePath = path.join(templatesDir, file);
-   const fileStat = fs.statSync(filePath);
-
-   // Process only files ending with .html
-   if (fileStat.isFile() && path.extname(file).toLowerCase() === '.html') {
-     const baseName = path.basename(file, '.html');
-     // Read file content
-     const content = fs.readFileSync(filePath, 'utf8');
-     // Escape backticks, backslashes, and ${} for template literal safety
-     const escapedContent = content
-                       .replace(/\\/g, '\\\\') // Escape backslashes
-                       .replace(/`/g, '\\`')  // Escape backticks
-                       .replace(/\$\{/g, '\\${'); // Escape literal ${
-     // Add to object string, using template literals (`) for content
-     templatesObject += `  "${baseName}": \`${escapedContent}\`,\n`;
-   }
- });
-
- // Remove trailing comma if any templates were added
- if (templatesObject.endsWith(',\n')) {
-   templatesObject = templatesObject.slice(0, -2) + '\n';
- }
-
- templatesObject += '};\n'; // Close the object
-
- // Ensure destination directory exists
-  if (!fs.existsSync(paths.templates.dest)) {
-     fs.mkdirSync(paths.templates.dest, { recursive: true });
+  const filePath = path.join(templatesDir, file);
+  const fileStat = fs.statSync(filePath);
+  if (fileStat.isFile() && path.extname(file).toLowerCase() === '.html') {
+   const baseName = path.basename(file, '.html');
+   const content = fs.readFileSync(filePath, 'utf8');
+   const escapedContent = content
+          .replace(/\\/g, '\\\\')
+          .replace(/`/g, '\\`')
+          .replace(/\$\{/g, '\\${');
+   templatesObject += `  "${baseName}": \`${escapedContent}\`,\n`;
   }
-
- // Write the generated JS file
+ });
+ if (templatesObject.endsWith(',\n')) {
+  templatesObject = templatesObject.slice(0, -2) + '\n';
+ }
+ templatesObject += '};\n';
+ if (!fs.existsSync(paths.templates.dest)) {
+   fs.mkdirSync(paths.templates.dest, { recursive: true });
+ }
  fs.writeFileSync(generatedTemplatesJsPath, templatesObject);
  console.log(`Generated ${paths.templates.generatedFile} from HTML templates.`);
  done();
 }
 
 
-// JS task - MODIFIED
+// JS task - (remains unchanged from previous version)
 function js() {
  return gulp
-   // --- Source vendor files, generated templates, and then other JS files ---
-   .src([
-       // Vendor files first (ensure order is correct for dependencies)
-       './vendor/jquery/jquery.min.js',
-       './vendor/bootstrap/js/bootstrap.bundle.min.js',
-       './vendor/jquery-easing/jquery.easing.min.js',
-       './vendor/easymde/easymde.min.js',
-       // *** START Hammer.js ADDITION ***
-       './vendor/hammerjs/hammer.min.js', // Include the copied Hammer.js file
-       // *** END Hammer.js ADDITION ***
-
-       // Then the generated templates file
-       generatedTemplatesJsPath,
-
-       // Then your custom JS files
-       paths.js.src,
-     ],
-     { allowEmpty: true }
-   )
-   .pipe(plumber())
-   .pipe(concat(paths.js.concatFile))
-   .pipe(header(banner, {
-     pkg: pkg
-   }))
-   .pipe(gulp.dest(paths.js.dest))
-   .pipe(uglify())
-   .pipe(rename({
-     suffix: paths.js.minSuffix
-   }))
-   .pipe(gulp.dest(paths.js.dest))
-   .pipe(browsersync.stream());
+  .src([
+    './vendor/jquery/jquery.min.js',
+    './vendor/bootstrap/js/bootstrap.bundle.min.js',
+    './vendor/jquery-easing/jquery.easing.min.js',
+    './vendor/easymde/easymde.min.js',
+    './vendor/hammerjs/hammer.min.js',
+    generatedTemplatesJsPath,
+    paths.js.src,
+   ],
+   { allowEmpty: true }
+  )
+  .pipe(plumber())
+  .pipe(concat(paths.js.concatFile))
+  .pipe(header(banner, {
+   pkg: pkg
+  }))
+  .pipe(gulp.dest(paths.js.dest))
+  .pipe(uglify())
+  .pipe(rename({
+   suffix: paths.js.minSuffix
+  }))
+  .pipe(gulp.dest(paths.js.dest))
+  .pipe(browsersync.stream());
 }
 
-// Watch files (remains unchanged)
+// Watch files - Watches generated font CSS
 function watchFiles() {
  gulp.watch("./scss/**/*.scss", css);
+ // Watch the generated font CSS file as well, trigger css task if it changes (e.g., after googlefonts runs)
+ gulp.watch(generatedFontCssPath, css);
  gulp.watch([paths.js.src, `!${generatedTemplatesJsPath}`], js);
  gulp.watch(paths.templates.src, gulp.series(generateTemplatesJS, js));
+ // Watch the font list file, regenerate fonts and then css if it changes
+ gulp.watch(paths.fonts.src, gulp.series(googlefonts, css));
  gulp.watch("./**/*.html", browserSyncReload);
 }
 
-// Define complex tasks (remains unchanged)
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, generateTemplatesJS, gulp.parallel(css, js));
+// Define complex tasks - Ensure googlefonts runs before css
+const vendor = gulp.series(clean, googlefonts, modules);
+const build = gulp.series(vendor, generateTemplatesJS, gulp.parallel(css, js)); // css now depends on googlefonts completing first
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// Export tasks (remains unchanged)
+// Export tasks
 exports.css = css;
 exports.js = js;
 exports.clean = clean;
@@ -254,3 +277,4 @@ exports.build = build;
 exports.watch = watch;
 exports.default = build;
 exports.generateTemplatesJS = generateTemplatesJS;
+exports.googlefonts = googlefonts;
