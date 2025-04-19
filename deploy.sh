42d491a0817a43ac1cdcafab1ba171c29a8fe14e
Aario Shahbany <aario.shahbany@gmail.com>
2025-04-19 13:22:25 +0200

    Fixed issue with deploy script uninstall

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ mode=release
 
 if [ "x$1" == 'xdebug' ]; then
     mode=debug
+    shift
 fi
 
+if [ "x$1" == 'xuninstall' ]; then
+    adb uninstall info.aario.snotepad
+fi
 
-adb uninstall info.aario.snotepad
 adb install ./app/build/outputs/apk/$mode/app-$mode.apk
 adb shell monkey -p info.aario.snotepad -c android.intent.category.LAUNCHER 1
